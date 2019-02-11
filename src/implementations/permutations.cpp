edf9fe0bb6b9909e9c214783c45e58e4fb077c49
Himanshu Chhabra <himichhabra14@gmail.com>
2019-02-11 11:16:33 +0530

    Sorted and unique permutations

--- src/implementations/permutations.cpp
+++ src/implementations/permutations.cpp
@@ -1,31 +1,40 @@
 /**
- * Problem: Permutations of a string.
+ * Problem: Unique Permutations of a string (works even if characters repeated).
  * 
  */
 #include<bits/stdc++.h>
 using namespace std;
 
+
 // Recursive
-void permutations(char* in,int i){
+void permutations(char* in,int i,set<string> &s){
     // Base case
     if(in[i]=='\0'){
-        cout<<in<<endl;
+        string temp = in;
+        s.insert(in);
         return;
     }
     
     // Replace in[i] with in[j] and keep on form permutations
     for(int j=i;in[j]!='\0';j++){
         swap(in[i],in[j]);
-        permutations(in, i+1);
+        permutations(in, i+1,s);
         swap(in[i],in[j]);
     }
 }
 
 int main() {
-    char in[] = "abc";
-    permutations(in,0);
+    char in[] = "aab";
     
+    // For storing unique permutations.
+    // By default, set also sorts items in it.
+    set<string> s;
     
+    permutations(in,0,s);
+    
+    for(auto i:s){
+        cout<<i<<endl;
+    }
     
     
 	return 0;
